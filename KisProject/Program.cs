a6f30ad823c4a0551dc38ae076fdac9025d02af7
Gembiczki Dániel <gembi1226@gmail.com>
2025-10-01 08:41:08 +0200

    Int létrehozva.

--- KisProject/Program.cs
+++ KisProject/Program.cs
@@ -17,6 +17,7 @@ namespace KisProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Számológép());
+            int a = 0;
         }
     }
 }
