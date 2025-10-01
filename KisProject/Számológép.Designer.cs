b982f3f309cdcd0940b7ad22bd3a507bb7702fc5
Radics Máté Mihály <radics.matemihaly@gmail.com>
2025-10-01 20:52:06 +0200

    Összes gomb és kijelzo hozzá adva.(Funkciok nem)

--- KisProject/Számológép.Designer.cs
+++ KisProject/Számológép.Designer.cs
@@ -39,13 +39,24 @@
             this.Number7 = new System.Windows.Forms.Button();
             this.Number0 = new System.Windows.Forms.Button();
             this.btnDecimal = new System.Windows.Forms.Button();
+            this.btnEquals = new System.Windows.Forms.Button();
+            this.btnMultiplication = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.btnModulo = new System.Windows.Forms.Button();
+            this.btnSingChangeKey = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSimpleClear = new System.Windows.Forms.Button();
+            this.Screen = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Number1
             // 
-            this.Number1.Location = new System.Drawing.Point(120, 297);
+            this.Number1.Location = new System.Drawing.Point(80, 193);
+            this.Number1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Number1.Name = "Number1";
-            this.Number1.Size = new System.Drawing.Size(99, 51);
+            this.Number1.Size = new System.Drawing.Size(66, 33);
             this.Number1.TabIndex = 0;
             this.Number1.Text = "1";
             this.Number1.UseVisualStyleBackColor = true;
@@ -53,99 +64,219 @@
             // 
             // Number2
             // 
-            this.Number2.Location = new System.Drawing.Point(225, 297);
+            this.Number2.Location = new System.Drawing.Point(150, 193);
+            this.Number2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Number2.Name = "Number2";
-            this.Number2.Size = new System.Drawing.Size(99, 51);
+            this.Number2.Size = new System.Drawing.Size(66, 33);
             this.Number2.TabIndex = 1;
             this.Number2.Text = "2";
             this.Number2.UseVisualStyleBackColor = true;
             // 
             // Number3
             // 
-            this.Number3.Location = new System.Drawing.Point(339, 297);
+            this.Number3.Location = new System.Drawing.Point(226, 193);
+            this.Number3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Number3.Name = "Number3";
-            this.Number3.Size = new System.Drawing.Size(99, 51);
+            this.Number3.Size = new System.Drawing.Size(66, 33);
             this.Number3.TabIndex = 2;
             this.Number3.Text = "3";
             this.Number3.UseVisualStyleBackColor = true;
             // 
             // Number6
             // 
-            this.Number6.Location = new System.Drawing.Point(339, 229);
+            this.Number6.Location = new System.Drawing.Point(226, 149);
+            this.Number6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Number6.Name = "Number6";
-            this.Number6.Size = new System.Drawing.Size(99, 51);
+            this.Number6.Size = new System.Drawing.Size(66, 33);
             this.Number6.TabIndex = 5;
             this.Number6.Text = "6";
             this.Number6.UseVisualStyleBackColor = true;
             // 
             // Number5
             // 
-            this.Number5.Location = new System.Drawing.Point(225, 229);
+            this.Number5.Location = new System.Drawing.Point(150, 149);
+            this.Number5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Number5.Name = "Number5";
-            this.Number5.Size = new System.Drawing.Size(99, 51);
+            this.Number5.Size = new System.Drawing.Size(66, 33);
             this.Number5.TabIndex = 4;
             this.Number5.Text = "5";
             this.Number5.UseVisualStyleBackColor = true;
             // 
             // Number4
             // 
-            this.Number4.Location = new System.Drawing.Point(120, 229);
+            this.Number4.Location = new System.Drawing.Point(80, 149);
+            this.Number4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Number4.Name = "Number4";
-            this.Number4.Size = new System.Drawing.Size(99, 51);
+            this.Number4.Size = new System.Drawing.Size(66, 33);
             this.Number4.TabIndex = 3;
             this.Number4.Text = "4";
             this.Number4.UseVisualStyleBackColor = true;
             // 
             // Number9
             // 
-            this.Number9.Location = new System.Drawing.Point(339, 163);
+            this.Number9.Location = new System.Drawing.Point(226, 106);
+            this.Number9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Number9.Name = "Number9";
-            this.Number9.Size = new System.Drawing.Size(99, 51);
+            this.Number9.Size = new System.Drawing.Size(66, 33);
             this.Number9.TabIndex = 8;
             this.Number9.Text = "9";
             this.Number9.UseVisualStyleBackColor = true;
             // 
             // Number8
             // 
-            this.Number8.Location = new System.Drawing.Point(225, 163);
+            this.Number8.Location = new System.Drawing.Point(150, 106);
+            this.Number8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Number8.Name = "Number8";
-            this.Number8.Size = new System.Drawing.Size(99, 51);
+            this.Number8.Size = new System.Drawing.Size(66, 33);
             this.Number8.TabIndex = 7;
             this.Number8.Text = "8";
             this.Number8.UseVisualStyleBackColor = true;
             // 
             // Number7
             // 
-            this.Number7.Location = new System.Drawing.Point(120, 163);
+            this.Number7.Location = new System.Drawing.Point(80, 106);
+            this.Number7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Number7.Name = "Number7";
-            this.Number7.Size = new System.Drawing.Size(99, 51);
+            this.Number7.Size = new System.Drawing.Size(66, 33);
             this.Number7.TabIndex = 6;
             this.Number7.Text = "7";
             this.Number7.UseVisualStyleBackColor = true;
             // 
             // Number0
             // 
-            this.Number0.Location = new System.Drawing.Point(120, 354);
+            this.Number0.Location = new System.Drawing.Point(150, 230);
+            this.Number0.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Number0.Name = "Number0";
-            this.Number0.Size = new System.Drawing.Size(204, 51);
+            this.Number0.Size = new System.Drawing.Size(66, 33);
             this.Number0.TabIndex = 9;
             this.Number0.Text = "0";
             this.Number0.UseVisualStyleBackColor = true;
             // 
             // btnDecimal
             // 
-            this.btnDecimal.Location = new System.Drawing.Point(339, 354);
+            this.btnDecimal.Location = new System.Drawing.Point(226, 230);
+            this.btnDecimal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDecimal.Name = "btnDecimal";
-            this.btnDecimal.Size = new System.Drawing.Size(99, 51);
+            this.btnDecimal.Size = new System.Drawing.Size(66, 33);
             this.btnDecimal.TabIndex = 10;
             this.btnDecimal.Text = ",";
             this.btnDecimal.UseVisualStyleBackColor = true;
             // 
+            // btnEquals
+            // 
+            this.btnEquals.Location = new System.Drawing.Point(296, 230);
+            this.btnEquals.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEquals.Name = "btnEquals";
+            this.btnEquals.Size = new System.Drawing.Size(66, 33);
+            this.btnEquals.TabIndex = 14;
+            this.btnEquals.Text = "=";
+            this.btnEquals.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiplication
+            // 
+            this.btnMultiplication.Location = new System.Drawing.Point(296, 106);
+            this.btnMultiplication.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMultiplication.Name = "btnMultiplication";
+            this.btnMultiplication.Size = new System.Drawing.Size(66, 33);
+            this.btnMultiplication.TabIndex = 13;
+            this.btnMultiplication.Text = "X";
+            this.btnMultiplication.UseVisualStyleBackColor = true;
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(296, 149);
+            this.btnMinus.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(66, 33);
+            this.btnMinus.TabIndex = 12;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(296, 193);
+            this.btnPlus.Margin = new System.Windows.Forms.Padding(2);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(66, 33);
+            this.btnPlus.TabIndex = 11;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Location = new System.Drawing.Point(296, 69);
+            this.btnDivision.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(66, 33);
+            this.btnDivision.TabIndex = 18;
+            this.btnDivision.Text = "÷";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            // 
+            // btnModulo
+            // 
+            this.btnModulo.Location = new System.Drawing.Point(226, 69);
+            this.btnModulo.Margin = new System.Windows.Forms.Padding(2);
+            this.btnModulo.Name = "btnModulo";
+            this.btnModulo.Size = new System.Drawing.Size(66, 33);
+            this.btnModulo.TabIndex = 17;
+            this.btnModulo.Text = "%";
+            this.btnModulo.UseVisualStyleBackColor = true;
+            // 
+            // btnSingChangeKey
+            // 
+            this.btnSingChangeKey.Location = new System.Drawing.Point(80, 230);
+            this.btnSingChangeKey.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSingChangeKey.Name = "btnSingChangeKey";
+            this.btnSingChangeKey.Size = new System.Drawing.Size(66, 33);
+            this.btnSingChangeKey.TabIndex = 16;
+            this.btnSingChangeKey.Text = "+/-";
+            this.btnSingChangeKey.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(80, 69);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(66, 33);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "AC";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnSimpleClear
+            // 
+            this.btnSimpleClear.Location = new System.Drawing.Point(150, 69);
+            this.btnSimpleClear.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSimpleClear.Name = "btnSimpleClear";
+            this.btnSimpleClear.Size = new System.Drawing.Size(66, 33);
+            this.btnSimpleClear.TabIndex = 19;
+            this.btnSimpleClear.Text = "C";
+            this.btnSimpleClear.UseVisualStyleBackColor = true;
+            // 
+            // Screen
+            // 
+            this.Screen.Location = new System.Drawing.Point(80, 27);
+            this.Screen.Multiline = true;
+            this.Screen.Name = "Screen";
+            this.Screen.ReadOnly = true;
+            this.Screen.Size = new System.Drawing.Size(282, 37);
+            this.Screen.TabIndex = 20;
+            this.Screen.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
             // Számológép
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(994, 450);
+            this.ClientSize = new System.Drawing.Size(663, 292);
+            this.Controls.Add(this.Screen);
+            this.Controls.Add(this.btnSimpleClear);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnModulo);
+            this.Controls.Add(this.btnSingChangeKey);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEquals);
+            this.Controls.Add(this.btnMultiplication);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
             this.Controls.Add(this.btnDecimal);
             this.Controls.Add(this.Number0);
             this.Controls.Add(this.Number9);
@@ -157,10 +288,12 @@
             this.Controls.Add(this.Number3);
             this.Controls.Add(this.Number2);
             this.Controls.Add(this.Number1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Számológép";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Számológép_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -177,6 +310,16 @@
         private System.Windows.Forms.Button Number7;
         private System.Windows.Forms.Button Number0;
         private System.Windows.Forms.Button btnDecimal;
+        private System.Windows.Forms.Button btnEquals;
+        private System.Windows.Forms.Button btnMultiplication;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnDivision;
+        private System.Windows.Forms.Button btnModulo;
+        private System.Windows.Forms.Button btnSingChangeKey;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSimpleClear;
+        private System.Windows.Forms.TextBox Screen;
     }
 }
 
