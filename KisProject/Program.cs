34c33e8d891a8a9e5bc41c7299e6c512086bd87d
Radics Máté Mihály <radics.matemihaly@gmail.com>
2025-10-01 08:31:26 +0200

    Project neve átírva

--- KisProject/Program.cs
+++ KisProject/Program.cs
@@ -16,7 +16,7 @@ namespace KisProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Számológép());
         }
     }
 }
